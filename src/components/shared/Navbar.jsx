60cf60aa59add602e23bfc3d17267adb237c951f
Tunan10 <tunansakib200@gmail.com>
2026-04-16 14:24:34 +0600

    implement responsive navbar with active state toggle for Home, Timeline, and Stats

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { FaHome } from 'react-icons/fa';
+import { IoMdTime } from 'react-icons/io';
+import { MdOutlineQueryStats } from 'react-icons/md';
+import { Link } from 'react-router';
+
+const Navbar = () => {
+
+  const [active, setActive] = useState("Home");
+
+  // 👉 আলাদা function
+  const handleToggle = (name) => {
+    setActive(name);
+  };
+
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm w-11/12 ">
+        
+        <div className="navbar-start">
+          <a className="btn btn-ghost text-xl">
+            <p className='font-bold'>
+              Keen<span className='font-semibold text-[#244D3F]'>Keeper</span>
+            </p>
+          </a>
+        </div>
+
+        <div className="navbar-center hidden lg:flex"></div>
+
+        <div className="navbar-end gap-2">
+          
+          <Link to={"/"}  >
+          <a
+            onClick={() => handleToggle("Home")}
+            className={`btn ${
+              active === "Home"
+                ? "bg-[#244D3F] text-white"
+                : ""
+            }`}
+          ><FaHome className=' text-xl '/>
+            Home
+          </a>
+          </Link>
+
+          <Link to={"/timeline"} >
+            <a
+            onClick={() => handleToggle("Timeline")}
+            className={`btn ${
+              active === "Timeline"
+                ? "bg-[#244D3F] text-white"
+                : ""
+            }`}
+          ><IoMdTime className='text-2xl'/>
+            Timeline
+          </a>
+          
+          </Link>
+
+          <Link to={"/stats"}>
+          <a
+            onClick={() => handleToggle("Stats")}
+            className={`btn ${
+              active === "Stats"
+                ? "bg-[#244D3F] text-white"
+                : ""
+            }`}
+          ><MdOutlineQueryStats className='text-2xl' />
+            Stats
+          </a>
+          
+          </Link>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,29 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router/dom';
 import { createBrowserRouter } from 'react-router';
+import RootLayout from './layout/RootLayout';
 
 const router = createBrowserRouter(
   [
     {
       path: '/',
-      element: <h1>Home</h1>,
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <h2>home</h2>
+
+        },
+        {
+          path: '/timeline',
+          element: <h2>hello im timeline</h2> 
+        },
+        {
+          path: '/stats',
+          element: <h2>hello im stats</h2>
+        }
+      ],
+      errorElement: <h2>404 not found</h2>
     }
 
   ]
